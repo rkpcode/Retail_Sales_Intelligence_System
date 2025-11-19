1f05dab404808ace64218e345fe646951cb511ad
rkpcode <rkpfarmx@gmail.com>
2025-11-19 16:59:06 +0530

    model trainer

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from src.Retail_Sale_Intelligent_System.logger import logging
 from src.Retail_Sale_Intelligent_System.exception import CustomException
 from src.Retail_Sale_Intelligent_System.components.data_ingestion import DataIngestion
 from src.Retail_Sale_Intelligent_System.components.data_transformation import DataTransformation
-#from src.Retail_Sale_Intelligent_System.components.model_trainer import ModelTrainerConfig,ModelTrainer
+from src.Retail_Sale_Intelligent_System.components.model_trainer import ModelTrainerConfig,ModelTrainer
 import sys
 
 if __name__ == "__main__":
@@ -21,9 +21,9 @@ if __name__ == "__main__":
         train_array, test_array, _ = data_transformation.initiate_data_transormation(train_data_path, test_data_path)
         
         # Model Training
-       # model_trainer = ModelTrainer()
-       # trained_model_score = model_trainer.initiate_model_trainer(train_array, test_array)
-       # logging.info(f"Model training completed. Score: {trained_model_score}")
+        model_trainer = ModelTrainer()
+        trained_model_score = model_trainer.initiate_model_trainer(train_array, test_array)
+        logging.info(f"Model training completed. Score: {trained_model_score}")
 
 
 
